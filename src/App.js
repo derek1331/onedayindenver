3072280beb3cd3028550cb45f1d8857dec36cfc0
Derek Reed <1331derek@gmail.com>
2018-10-18 11:35:56 -0600

    react-materialize

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import Nav from "./components/Nav"
+import Hello from "./components/Nav"
 import Footer from "./components/Footer"
 import Home from "./components/Home"
+
 
 
 
@@ -10,7 +11,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
+        <Hello />
+ 
         <Home />
         <Footer />
 

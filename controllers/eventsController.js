c9bce86101d54fe72397b8e607f40e93aa492210
Derek Reed <1331derek@gmail.com>
2018-10-24 22:11:32 -0600

    api routes fix

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -2,19 +2,19 @@ const db = require("../models");
 
 module.exports = {
     findAll: function(req, res) {
-        db.Event
+        db.Events
         .find(req.query)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
-        db.Event
+        db.Events
         .create(req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
-        db.Event
+        db.Events
         .findById({_id: req.params.id})
         .then(dbModel => dbModel.remove())
         .then(dbModel => res.json(dbModel))

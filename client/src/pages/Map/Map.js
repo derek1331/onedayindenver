cfd849a2f8d3290644e420d8f55d40c46980d25f
ashalkey <alanshalkey@gmail.com>
2018-10-27 13:41:09 -0600

    getting google-maps-react container rendering, testing placing markers

--- client/src/pages/Map/Map.js
+++ client/src/pages/Map/Map.js
@@ -5,14 +5,17 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 export class MapContainer extends Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map google={this.props.google} zoom={14}
+        initialCenter={{lat: 39.9626, lng: -76.7277}}>
 
         <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
+        <Marker name={'test marker'}
+          position={{lat:39.948765, lng: -76.671208}}/>
 
         <InfoWindow onClose={this.onInfoWindowClose}>
             <div>
-              <h1>{this.state.selectedPlace.name}</h1>
+              <h1>test</h1>
             </div>
         </InfoWindow>
       </Map>
--- client/src/pages/Testing/Testing.js
+++ client/src/pages/Testing/Testing.js
@@ -1,26 +1,40 @@
 import React, {Component} from "react";
 import API from "../../utils/API";
 import axios from "axios-jsonp-pro";
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class Testing extends Component {
-    axiosRequest () {
+    axiosRequest() {
         console.log("been clicked");
-        axios.jsonp(`https://localhost:3000/api/events` , {})
-        .then(res => {
+        axios.jsonp(`https://localhost:3000/api/events`, {})
+            .then(res => {
 
-            console.log(res);
+                console.log(res);
 
-        });
+            });
     };
 
     render() {
         return (
-        <div>
-        <h1>This is a test</h1>
-        <button onClick={this.axiosRequest}>This is a button</button>
-        </div>
+            <div>
+                <h1>This is a test</h1>
+                <button onClick={this.axiosRequest}>This is a button</button>
+                <Map google={this.props.google} zoom={14}>
+
+                    <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                        <div>
+                            <h1>test</h1>
+                        </div>
+                    </InfoWindow>
+                </Map>
+            </div>
         );
     }
 }
 
-export default Testing;
+export default GoogleApiWrapper({
+    apiKey: "AIzaSyBhNoh-8XLeci7x7IWHfIGXuxcp1djJfq8"
+})(Testing);

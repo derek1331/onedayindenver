98233799d7ce41204932343d9cb369e6ba664676
ashalkey <alanshalkey@gmail.com>
2018-10-31 20:03:28 -0600

    modifications

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,17 +1,14 @@
 const router = require("express").Router();
 const eventRoutes = require("./events");
-<<<<<<< HEAD
 const signInRoute = require("./signin");
 
 router.use("/events", eventRoutes);
 router.use("/signin", signInRoute);
-=======
 const userRoutes = require("./users");
 const loginRoutes = require("./login");
 
 router.use("/events", eventRoutes);
 router.use("/users", userRoutes);
 router.use("/login", loginRoutes);
->>>>>>> 9255454cfc3d60643ec2539e959559d3359cbb36
 
 module.exports = router;
